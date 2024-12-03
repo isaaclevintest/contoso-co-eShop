4ef457dcae8a4bed32509e1b314296f854107091
isaaclevintest <162737061+isaaclevintest@users.noreply.github.com>
2024-12-03 13:22:40 -0800

    test

--- src/WebApp/AIOptions.cs
+++ src/WebApp/AIOptions.cs
@@ -11,6 +11,9 @@ public class OpenAIOptions
     /// <summary>The name of the chat model to use.</summary>
     /// <remarks>When using Azure OpenAI, this should be the "Deployment name" of the chat model.</remarks>
     public string ChatModel { get; set; } = "gpt-3.5-turbo-16k";
+
+
+    
 }
 
 
